64e975e03bccb035ed844bd3a9b4f3357fe785c1
ajorgefsa <ajorge@fsa-ltda.com>
2025-03-20 11:39:23 -0400

    Complete translations for Service Order Tasks Documents component.

--- RazorClassLibrary1/Pages/ServiceOrderTaskDocument.razor.cs
+++ RazorClassLibrary1/Pages/ServiceOrderTaskDocument.razor.cs
@@ -116,7 +116,7 @@ namespace RazorClassLibrary1.Pages
                         {
                             Action = GridGeneralActions.ADD_ITEM,
                             Icon = "add",
-                            Title = "Add Service Order Task Document",
+                            Title = @Localizer["AddServiceOrderTaskDocument"],
                             Style = ButtonStyle.Primary.GetHashCode(),
                             //Show = show => { return create; }
                         }
