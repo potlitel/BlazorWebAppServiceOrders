454eaf6aa3c06d7ca546908596fdda38c942531f
ajorgefsa <ajorge@fsa-ltda.com>
2025-04-18 16:08:21 -0400

    Update AddEdditSOSupplyComponent.

--- RazorClassLibrary1/Pages/SO_Supply/AddEditSOSupplyComponent.razor.cs
+++ RazorClassLibrary1/Pages/SO_Supply/AddEditSOSupplyComponent.razor.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Components;
 using Radzen;
 using RazorClassLibrary1.Dtos;
+using RazorClassLibrary1.Services.HttpClientSrv.ServiceOrderTypes.GetAll;
 
 
 namespace RazorClassLibrary1.Pages.SO_Supply
@@ -38,27 +39,37 @@ namespace RazorClassLibrary1.Pages.SO_Supply
 
                 //https://github.com/bchavez/Bogus
                 //https://medium.com/@fahrican.kcn/generating-mock-data-in-net-applications-a-comprehensive-guide-to-using-bogus-3fd60cc09f18#id_token=eyJhbGciOiJSUzI1NiIsImtpZCI6IjkxNGZiOWIwODcxODBiYzAzMDMyODQ1MDBjNWY1NDBjNmQ0ZjVlMmYiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiIyMTYyOTYwMzU4MzQtazFrNnFlMDYwczJ0cDJhMmphbTRsamRjbXMwMHN0dGcuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiIyMTYyOTYwMzU4MzQtazFrNnFlMDYwczJ0cDJhMmphbTRsamRjbXMwMHN0dGcuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMTU2MzUxODk4NjQzOTMyNzIzMDIiLCJlbWFpbCI6InBvdGxpdGVsQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJuYmYiOjE3NDE3MDc1MTMsIm5hbWUiOiJBbGFpbiBKb3JnZSIsInBpY3R1cmUiOiJodHRwczovL2xoMy5nb29nbGV1c2VyY29udGVudC5jb20vYS9BQ2c4b2NMajJCbE9zZmZsM21IQWUxREd3dGlkUFkxdDF5cEpMM3U5aWxuWGwzOHAzN0tzUkp2bz1zOTYtYyIsImdpdmVuX25hbWUiOiJBbGFpbiIsImZhbWlseV9uYW1lIjoiSm9yZ2UiLCJpYXQiOjE3NDE3MDc4MTMsImV4cCI6MTc0MTcxMTQxMywianRpIjoiM2UyMDYyZTY3MzJmNzBmODgzY2JjMjUzYjE0ZTcxM2RhNzI3YmY2NCJ9.GDPLzC40vJtA0hCz9whdNZSJ8DzPPJQcGeWQY1fBqPWoS3VVH0soqoYlvg_bAexkZkH1X9RiDd7CmJ02wvxQhQSJIdCMMUH2xUMsu5bqJOD3koiWQ7J44dBHMlyMwlZ18cIgZHMvRPgQUH-Z6yEp8viggoCG83mllEnTOH8DobsFQxqstYhMfmJix_THkIfuLRqxcxSlZxxJwvYEq4CxDwHS0Wx3cQbSL3sPWVpUx4OBfEOdafeLb_4Q2ISUSEZjOhbPJrliIyUBWhoNn0UF1I8UgWd3bzPpTutuK6NSpukoKAKC6I42U8U6pStb7V_3Dosp2YEHhNwz_KEkExzp7w
-                
-                SupplyOperations = new Faker<SupplyOperationDto>()
-                                        .RuleFor(x => x.Code, f => f.Finance.Account(15))
-                                        .RuleFor(x => x.Description, f => f.Address.FullAddress())
-                                        .Generate(50).ToList();
 
-                ServiceOrderTasks = new Faker<ServiceOrderTaskDto>()
-                                        .RuleFor(x => x.Observations, f => f.Address.FullAddress())
-                                        .Generate(50).ToList();
+                var supplyOperationsTask = GetAllSupplyOperationsService.Handle(null);
+                var serviceOrdersTasksTask = GetAllServiceOrdersTasksService.Handle(null);
 
-                //ServiceOrders = faker
-                //                     .RuleFor(x => x.Number, f => f.Finance.Account(15))
-                //                     .RuleFor(x => x.EstimatedEndingDate, f => f.Date.Future(1))
-                //                     .RuleFor(x => x.Observations, f => f.Address.FullAddress())
-                //                     .RuleFor(x => x.Address, f => f.Address.FullAddress())
-                //                     .Generate(50).ToList();
+                var supplyOperations = await supplyOperationsTask;
+                var serviceOrdersTasks = await serviceOrdersTasksTask;
 
-                //DocumentTypes = fakerDoc
-                //                     .RuleFor(x => x.Code, f => f.Finance.Bic())
-                //                     .RuleFor(x => x.Description, f => f.Lorem.Sentence(8))
-                //                     .Generate(15).ToList();
+                if (supplyOperations?.Succeeded == true)
+                {
+                    SupplyOperations = supplyOperations.Data!.ToList();
+                    if (Supply.SupplyOperationId == 0)
+                        Supply.SupplyOperation = SupplyOperations.First();
+                }
+
+                if (serviceOrdersTasks?.Succeeded == true)
+                {
+                    ServiceOrderTasks = serviceOrdersTasks.Data!.ToList();
+                    if (Supply.ServiceOrderTaskId == 0)
+                        Supply.ServiceOrderTask = ServiceOrderTasks.First();
+                }
+
+                #region ToDelete
+
+                //SupplyOperations = new Faker<SupplyOperationDto>()
+                //                        .RuleFor(x => x.Code, f => f.Finance.Account(15))
+                //                        .RuleFor(x => x.Description, f => f.Address.FullAddress())
+                //                        .Generate(50).ToList();
+
+                //ServiceOrderTasks = new Faker<ServiceOrderTaskDto>()
+                //                        .RuleFor(x => x.Observations, f => f.Address.FullAddress())
+                //                        .Generate(50).ToList();
 
                 //var policyGroupsTask = GetAllPolicyGroupsService.Handle(null);
                 //var policyGroups = await policyGroupsTask;
@@ -69,6 +80,8 @@ namespace RazorClassLibrary1.Pages.SO_Supply
                 //    if (Policy.PolicyGroupId == 0)
                 //        Policy.PolicyGroup = PolicyGroups.First();
                 //}
+
+                #endregion
             }
             catch (UnauthorizedAccessException) { }
             catch (Exception ex)
