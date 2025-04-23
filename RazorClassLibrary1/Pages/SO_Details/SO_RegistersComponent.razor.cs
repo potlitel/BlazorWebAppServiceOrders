975a39e64d9aebe998b048bc9a7e37a41c529249
ajorgefsa <ajorge@fsa-ltda.com>
2025-04-23 12:21:17 -0400

    Refactoring service order details component.

--- RazorClassLibrary1/Pages/SO_Details/SO_RegistersComponent.razor.cs
+++ RazorClassLibrary1/Pages/SO_Details/SO_RegistersComponent.razor.cs
@@ -12,7 +12,7 @@ namespace RazorClassLibrary1.Pages.SO_Details
     public partial class SO_RegistersComponent
     {
 
-        [Parameter]
+        [CascadingParameter]
         public ServiceOrderDto ServiceOrder { get; set; } = new();
 
 
